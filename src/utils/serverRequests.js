fdcd0737e1eac5914b84c5b8067ee3fbafe53b6c
LongWyrsch <long.nqw@gmail.com>
2023-01-18 09:11:21 +0100

    Wrap try/catch around fetch to catch network errors

--- src/utils/serverRequests.js
+++ src/utils/serverRequests.js
@@ -1,20 +1,25 @@
-export const serverRequests = async (reqPath, reqMethod, reqBody, navigateAction, serverFailureAction) => {
-	const response = await fetch(`http://localhost:3000${reqPath}`, {
-		method: reqMethod,
-		credentials: 'include',
-		headers: {
-			'Content-type': 'application/json',
-		},
-		// We convert the React state to JSON and send it as the POST body
-		body: JSON.stringify(reqBody),
-	});
+export const serverRequests = async (reqPath, reqMethod, reqBody, navigate, path, serverFailureAction) => {
+	let response
+	try {
+		response = await fetch(`http://localhost:3000${reqPath}`, {
+			method: reqMethod,
+			credentials: 'include',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			// We convert the React state to JSON and send it as the POST body
+			body: JSON.stringify(reqBody),
+		});
+	} catch (error) {
+		navigate('/home/error')
+		serverFailureAction();
+	} 
 	if (response.status === 200) {
-		// Not need to do anything. Slice equals databaase.
-		// dispatch(getRecipes());
+		return response.json()
 	} else if (response.status === 401) {
-		console.log('failed')
 		window.alert('Failed to authenticate');
-		navigateAction();
+		navigate(path);
+		console.log('failed')
 	} else {
 		serverFailureAction();
 		window.alert('Server error. Please try again.');
