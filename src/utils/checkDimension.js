d53c36226f50067bbf38ceb9ed02d383ded77cf2
LongWyrsch <long.nqw@gmail.com>
2023-02-05 10:52:29 +0100

    Add validations to layouts in checkDimension()

--- src/utils/checkDimension.js
+++ src/utils/checkDimension.js
@@ -4,14 +4,22 @@ export const checkDimension = (layouts) => {
 	let checkedLayouts = layouts;
 
 	const checkPosition = (position) => {
-		if (position.w === 1 || position.h === 1) {
-            console.log('checkDimension => clearing layouts')
-            checkedLayouts = {}
-        };
+		// Check that card sizes are not indeed int and >= 2. Otherwise grid layout will crash. 
+		if (Number.isInteger(position.w) && Number.isInteger(position.h) && position.w >= 2 && position.h >= 2) {
+			return
+		};
+		console.log('checkDimension => clearing layouts')
+		checkedLayouts = {}
 	};
 
 	for (var value of Object.values(layouts)) {
-		value.forEach((position) => checkPosition(position));
+		value.forEach((position) => {
+			checkPosition(position)
+		});
+
+		// If checkedLayouts was set to {}, that means there was an invalid value so all layouts are discarded.
+		// The loop is therefore terminated. generateLayouts() will be called.
+		if (Object.keys(checkedLayouts).length === 0) {break}
 	}
 
 	return checkedLayouts;
