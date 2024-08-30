62226de35524a37fa7f0c1268954282c4eb23ad2
LongWyrsch <long.nqw@gmail.com>
2024-08-30 19:22:50 +0200

    chore: Update server URL in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const production = {
 	client_url: 'https://mygrocerylists.netlify.app',
-	server_url: 'https://mygrocerylists.up.railway.app'
+	server_url: 'https://grocery-list-backend-chf6hbgrgeh7f3bu.canadaeast-01.azurewebsites.net'
 };
 
 const development = {
